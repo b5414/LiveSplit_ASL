a2482d256719787681ac24f7e4199adbdee2a606
b5414 <mr.b5414@yandex.ru>
2021-03-08 18:21:57 +0800

    10

---
+++ other/gforce/gforce.cs
@@ -0,0 +1,27 @@
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+BATTERIES last map: 0508FF4C
+0508E480 + 1ACC
+
+int lastMapBats: "GForce.exe", 0x00512B4C, 0x4, 0x140, 0x1ACC;
+int lastMapBats: "GForce.exe", 0x00515750, 0x110, 0x3C, 0x1ACC; // good one
+
+0 = 0
+4 = 4
+
+// Also, plus one on Fly-Mine explode into bat spot
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int mapID: "GForce.exe", 0x00512498;
+
+0 level = training room
+1 to 26 = levels
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int aState: "GForce.exe", 0x00511D48;
+
+// possibly useful for Game Time
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
