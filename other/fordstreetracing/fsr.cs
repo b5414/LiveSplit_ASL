61b54735e3176f605d4f4a28db041d8e0b332c94
b5414 <mr.b5414@yandex.ru>
2021-04-20 00:57:38 +0800

    38 - FordStreetRacing stuff

---
+++ other/fordstreetracing/fsr.cs
@@ -0,0 +1,38 @@
+//
+//
+// Menu state:
+
+fsr.exe+4B2BD8
+
+51 90 = not start
+33 34 = not start challenges
+64 74 (sometimes 6) 64 = reset 
+
+103 104 (58 57 60 1 menu) = split
+1] 72 87 101 102 [103
+
+72 71 91
+71 91 = split if option, split by row
+
+//
+
+66 pause
+71 complete race pack, result screen
+53 garage
+79 saloon
+88 89 menu
+64 main menu
+86 pre main menu, title screen
+90 team racing menu, where challenges, solo, team, training
+51 pre team racing = tooltip
+82 solo cup
+
+59 = Reslut table for single career
+
+//
+//
+// Money:
+
+fsr.exe+4D95D8 + 50
+fsr.exe+19D518 + 50
+
